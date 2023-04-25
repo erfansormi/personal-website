fd37a2bfe934a09b7cfd20d0e27a24ac977c77e2
Erfan <erfansormi97@gmail.com>
2023-04-25 17:45:13 +0330

    added i18next and vazirmatn font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { appWithTranslation } from "next-i18next";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// font
+import { Vazirmatn } from 'next/font/google';
+export const vazir = Vazirmatn({
+  weight: ["300", "400", "500", "600", "700", "900"],
+  variable: '--font-vazir',
+  subsets: ['latin', "arabic", "latin-ext"],
+})
+
+// components
+import MantineTheme from '@/theme/mantineProvider'
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <MantineTheme>
+      <main className={`${vazir.className} ${vazir.variable}`}>
+        <Component {...pageProps} />
+      </main>
+    </MantineTheme>
+  )
 }
+
+export default appWithTranslation(App);
