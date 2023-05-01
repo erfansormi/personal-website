5e868f5779f9a24ca90f9b8c3338b02f6720b9cf
Erfan <erfansormi97@gmail.com>
2023-05-01 13:07:26 +0330

    Create skills.tsx

---
+++ src/components/pages/home/skills.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useTranslation } from 'next-i18next';
+
+// data
+import { skillsData } from './contentData';
+
+// ts
+import { Locales } from '@/types/locales';
+
+// components
+import SectionTitle from '@/components/data_display/sectionTitle';
+import SkillCard from '@/components/data_display/skillCard';
+
+const Skills = () => {
+    const { t } = useTranslation("home");
+
+    return (
+        <div>
+            <SectionTitle title={t("skills.title")} />
+            <div className='grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
+                {
+                    skillsData[t("locale") as Locales].map((item, index) =>
+                        <section key={index * 17}>
+                            <SkillCard image={item.image} level={item.level} title={item.title} />
+                        </section>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Skills;
