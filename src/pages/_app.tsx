0a06339f8beb0ee285e3ce4294bddc4a03662314
Erfan <erfansormi97@gmail.com>
2023-05-15 13:44:42 +0330

    added some meta tags and added json ld schema

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,7 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app'
 import '@/styles/globals.css'
-import { appWithTranslation } from "next-i18next";
+import { appWithTranslation, useTranslation } from "next-i18next";
 import { Analytics } from '@vercel/analytics/react';
 
 // font
@@ -17,20 +16,42 @@ export const vazir = Vazirmatn({
 import MantineTheme from '@/theme/mantineProvider'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const { locale } = useRouter();
+  const { t } = useTranslation("home");
+
+  // json ld schema
+  const structuredData = {
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    headline: t("name"),
+    name: t("name"),
+    description: t("description"),
+    email: "erfansormi97@gmail.com",
+    jobTitle: "Frontend Developer",
+    nationality: "Iranian",
+    url: "https://erfansormi.ir",
+    image: "https://www.erfansormi.ir/_next/image?url=%2Ferfansormi-1.jpg&w=128&q=85",
+    gender: "Male",
+    author: [
+      {
+        '@type': 'Person',
+        name: t("name"),
+      },
+    ],
+    datePublished: '2023-05-01T09:00:00.000Z',
+  };
 
   return (
     <>
       <Head>
         {/* Recommended Meta Tags */}
-        <meta name="language" content={locale === "fa" ? "persian" : "english"} />
-        <meta name="author" content='Erfan Sormi' />
-        <meta name="publisher" content='Erfan Sormi' />
-        <meta name="designer" content='Erfan Sormi' />
+        <meta name="language" content={t("name") as string} />
+        <meta name="author" content={t("name") as string} />
+        <meta name="publisher" content={t("name") as string} />
+        <meta name="designer" content={t("name") as string} />
 
         {/* Search Engine Optimization Meta Tags */}
-        <title>Erfan Sormi</title>
-        <meta name="description" content="Learn more about Erfan Sormi and his skills, projects and resume" />
+        <title>{t("name")}</title>
+        <meta name="description" content={t("description") as string} />
         <meta name="keywords" content="Frontend Developer,React Developer,Next js Developer,توسعه دهنده فرانت اند,توسعه دهنده ریکت,طراح سایت,توسعه دهنده سایت" />
 
         {/* Optional Meta Tags */}
@@ -43,8 +64,21 @@ const App = ({ Component, pageProps }: AppProps) => {
         <meta name="HandheldFriendly" content="true" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+        {/* og meta tags */}
+        <meta name="og:locale" content={t("locale") as string} />
+        <meta name="og:site_name" content='erfan sormi personal website' />
+        <meta name="og:title" content={t("name") as string} />
+
         {/* links */}
         <link rel="shortcut icon" href="/es.png" type="image/x-icon" />
+        <link rel="canonical" href="https://erfansormi.ir" />
+
+        {/* json ld schemas */}
+        <script
+          key="structured-data"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+        />
       </Head>
 
       <MantineTheme>
